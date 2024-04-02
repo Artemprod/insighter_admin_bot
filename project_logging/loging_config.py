985aaa955f1f742d888fcc91fb181405ff95a698
artemprod <artemprod1@gmail.com>
2024-04-02 19:36:08 +0400

    update env path   ,test stage AWS 6

--- project_logging/loging_config.py
+++ project_logging/loging_config.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 def load_loguru():
     env: Env = Env()
-    env.read_env(".env")
+    env.read_env("deploy/.env")
     logtail_source_token = env("LOGTAIL_INSIGHTER_ADMIN_SOURCE")
     logtail_handler = LogtailHandler(source_token=logtail_source_token)
     logger.add(
