2e43367798f37d9f6798334d7db8b9a49d02e4c2
artemprod <artemprod1@gmail.com>
2024-04-06 13:49:22 +0400

    update env in workflow env stage AWS 31

--- admin_insight_bot_container.py
+++ admin_insight_bot_container.py
@@ -2,6 +2,6 @@ from config.bot_configs import Config, load_bot_config
 from DB.Mongo.mongo_db import MongoAssistantRepositoryORM, MongoORMConnection
 
 
-config_data: Config = load_bot_config('.env')
+config_data: Config = load_bot_config('deploy/.env')
 MongoORMConnection(config_data.data_base, system_type=config_data.system.system_type)
 assistant_repository = MongoAssistantRepositoryORM()
