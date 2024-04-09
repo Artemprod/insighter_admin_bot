0450ca4dbee4b71a1d0b1fa28c5ea94a7390c6b5
artemprod <artemprod1@gmail.com>
2024-04-09 15:56:38 +0400

    update env in workflow env stage add new test token AWS 32

--- admin_insight_bot_container.py
+++ admin_insight_bot_container.py
@@ -2,6 +2,6 @@ from config.bot_configs import Config, load_bot_config
 from DB.Mongo.mongo_db import MongoAssistantRepositoryORM, MongoORMConnection
 
 
-config_data: Config = load_bot_config('deploy/.env')
+config_data: Config = load_bot_config('.env')
 MongoORMConnection(config_data.data_base, system_type=config_data.system.system_type)
 assistant_repository = MongoAssistantRepositoryORM()
