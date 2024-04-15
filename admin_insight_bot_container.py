771e2c3236ecf88458670e93fbeb408a53259780
artemprod <artemprod1@gmail.com>
2024-04-15 12:52:06 +0400

    deploy main 2

--- admin_insight_bot_container.py
+++ admin_insight_bot_container.py
@@ -5,3 +5,5 @@ from DB.Mongo.mongo_db import MongoAssistantRepositoryORM, MongoORMConnection
 config_data: Config = load_bot_config('.env')
 MongoORMConnection(config_data.data_base, system_type=config_data.system.system_type)
 assistant_repository = MongoAssistantRepositoryORM()
+
+
